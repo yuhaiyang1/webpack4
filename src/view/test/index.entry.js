e7a756de86463528b526c8cbae448292c37af075
yhy <>
2018-07-31 10:25:04 +0800

    提交配置文件

--- src/view/test/index.entry.js
+++ src/view/test/index.entry.js
@@ -3,16 +3,44 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 import 'isomorphic-fetch'
 class App extends Component {
+  static defaultProps = {
+    number: 1
+  }
+  // static propTypes = {
+  //   number: React.PropTypes.number.isRequired,
+  // }
   constructor (props) {
     super(props)
     this.state = {
       loading: false
     }
   }
+  componentWillMount () {
+    console.log('Component WILL MOUNT!')
+  }
+  componentDidMount () {
+    console.log('Component DID MOUNT!')
+  }
+  shouldComponentUpdate (newProps, newState) {
+    return true
+  }
+  componentWillUpdate (nextProps, nextState) {
+    console.log('Component WILL UPDATE!')
+  }
+  compnentDidUpdate (prevProps, prevState) {
+    console.log('Component DID UPDATE!')
+  }
+  componentWillUnmount () {
+    console.log('Component WILL UNMOUNT!')
+  }
+  componentDidCatch () {
+    console.log('报错了')
+  }
   render () {
+    // console.log(this)
     return (
       <div>
-        hello
+        {this.props.number}
       </div>
     )
   }
